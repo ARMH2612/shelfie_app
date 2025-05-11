62ee666bdcebbc96676226d98310e6653e7d5e96
Houssame ARMH <houssamerahiche@gmail.com>
2025-05-11 20:41:27 +0200

    Implement user authentication functions in UserContext and update Appwrite configuration for environment variables

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,12 +1,26 @@
 import { createContext, useState } from "react";
+import { account } from "../lib/appwrite";
+import { ID } from "react-native-appwrite";
 
 export const UserContext = createContext();
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
 
-  async function login(email, password) {}
-  async function register(email, password) {}
+  async function login(email, password) {
+    try {
+      await account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  async function register(email, password) {
+    try {
+      await account.create(ID.unique(), email, password);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   async function logout() {}
 
   return (
