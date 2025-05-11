65f361dd72ae1178163e18bbfea7121a2393e401
Houssame ARMH <houssamerahiche@gmail.com>
2025-05-11 12:44:11 +0200

    Refactor Register component to use themed layout and navigation link to Login

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,14 +1,42 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import ThemedView from "../../components/ThemedView";
+import Spacer from "../../components/Spacer";
+import ThemedText from "../../components/ThemedText";
+import { Link } from "expo-router";
 
 const Register = () => {
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <Spacer />
+      <ThemedText title={true} style={styles.title}>
+        Register for an account
+      </ThemedText>
+
+      <Spacer height={100} />
+      <Link href="/login">
+        <ThemedText
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Login instead
+        </ThemedText>
+      </Link>
+    </ThemedView>
   );
 };
 
 export default Register;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 18,
+    marginBottom: 30,
+  },
+});
