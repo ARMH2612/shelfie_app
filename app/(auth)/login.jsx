313d8e08768bdfd357e50027ad080fe7be0da3d7
Houssame ARMH <houssamerahiche@gmail.com>
2025-05-11 12:58:04 +0200

    Add Pressable button to Login component and update styles for interaction

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,8 +1,9 @@
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
 import { Link } from "expo-router";
+import { Colors } from "../../constants/Colors";
 
 const Login = () => {
   return (
@@ -11,6 +12,11 @@ const Login = () => {
       <ThemedText title={true} style={styles.title}>
         Login to your account
       </ThemedText>
+      <Pressable
+        style={({ pressed }) => [styles.btn, pressed && styles.pressed]}
+      >
+        <ThemedText style={{ color: "#f2f2f2" }}>Login</ThemedText>
+      </Pressable>
 
       <Spacer height={100} />
       <Link href="/register">
@@ -37,5 +43,13 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30,
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
   },
 });
